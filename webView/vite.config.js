2eaddbac27097639dfa2bf4bd1917ebaa2f9d2b1
Muhammad Taha Khan <mtaha.dev@gmail.com>
2025-10-05 22:19:56 +0100

    updated

--- webView/vite.config.js
+++ webView/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+   server: {
+    host: '0.0.0.0',
+    allowedHosts: ['ec2-3-87-119-123.compute-1.amazonaws.com'],
+    port: 5173
+  },
 })
