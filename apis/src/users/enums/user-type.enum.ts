c9adcda06601374241eb74d9097e2fbb27e941df
Muhammad Taha Khan <mtaha.dev@gmail.com>
2025-10-22 05:04:24 +0100

    updated

--- apis/src/users/enums/user-type.enum.ts
+++ apis/src/users/enums/user-type.enum.ts
@@ -2,7 +2,11 @@
 export enum UserType {
   ADMIN = 'admin',
   SUBSCRIBER = 'subscriber',
-  BUYER = 'buyer',
-  SELLER = 'seller',
-  MA_EXPERT = 'm&a_expert', // underscore instead of space (recommended)
+  BUYER_BASIC = 'buyer_basic',
+  buyer_PREMIUM = 'buyer_premium',
+  SELLER_BASIC = 'seller_basic', //like subscriber
+  SELLER_LISTING = 'seller_listing',
+  SELLER_CENTRAL = 'seller_central',
+  MA_EXPERT_BASIC = 'm&a_expert_basic', // underscore instead of space (recommended)
+  MA_EXPERT_PREMIUM = 'm&a_expert_premium', // underscore instead of space (recommended)
 }
