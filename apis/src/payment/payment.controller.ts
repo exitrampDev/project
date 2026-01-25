8e2e52083906921aed3ec3dc4ec24462b2fed869
Muhammad Taha Khan <mtaha.dev@gmail.com>
2026-01-25 06:38:43 +0000

    work for payment

--- apis/src/payment/payment.controller.ts
+++ apis/src/payment/payment.controller.ts
@@ -178,6 +178,12 @@ export class PaymentController {
       else if(user.role == 'seller_central'){
         amount = 60; //60 USD for premium sellers
       }
+      else if(user.role == 'seller_individual'){
+        amount = 30; //60 USD for premium sellers
+      }
+      else if(user.role == 'seller_broker'){
+        amount = 60; //60 USD for premium sellers
+      }
     
     const session = await this.paymentsService.createCheckoutSession(
       amount, userId,
