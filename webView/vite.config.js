f439d8ee9ce350a82e069ac8705c67247cbde96c
Muhammad Taha Khan <mtaha.dev@gmail.com>
2026-02-03 08:04:43 +0000

    update base folder

--- webView/vite.config.js
+++ webView/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-   base: '/exitramp/',
+  //  base: '/exitramp/',
    plugins: [react()],
    server: {
     host: '0.0.0.0',
