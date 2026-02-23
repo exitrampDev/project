41059add2e006979fce6b8ee76560dc1d5b29764
Muhammad Taha Khan <mtaha.dev@gmail.com>
2026-02-23 12:19:07 +0000

    udpate dependencies

--- apis/src/cron/cron.module.ts
+++ apis/src/cron/cron.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { CronService } from './cron.service';
 import { BusinessListingModule } from 'src/business-listing/business-listing.module';
 import { UsersModule } from 'src/users/users.module';
+import { PaymentModule } from 'src/payment/payment.module';
 
 @Module({
-    imports: [BusinessListingModule, UsersModule],
+    imports: [BusinessListingModule, UsersModule, PaymentModule],
     providers: [CronService],
 })
 export class CronModule {}
