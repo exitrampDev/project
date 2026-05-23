7a716678991f6e2e09f9b8578d332f9e4e06d506
Muhammad Taha Khan <mtaha.dev@gmail.com>
2026-05-23 09:40:07 +0100

    work for validation

--- apis/src/blog/dto/create-blog.dto.ts
+++ apis/src/blog/dto/create-blog.dto.ts
@@ -1,8 +1,10 @@
 import { IsNotEmpty, IsString, IsOptional } from 'class-validator';
+import { UniqueInCollection } from 'src/common/decorators/unique-in-collection.validator';
 
 export class CreateBlogDto {
   @IsOptional() 
   @IsString()
+ @UniqueInCollection({ collection: 'blogs', field: 'pageSlug', message: 'slug must be unique' })
   pageSlug!: string;
 
   @IsNotEmpty()
