22e7ba219b0233c57f5642965990bfeea80f70d6
exitrampDev <devteam@exitramp.co>
2026-08-13 14:43:47 +0500

    multiple time payment issue resolved

--- webView/src/main.jsx
+++ webView/src/main.jsx
@@ -6,11 +6,10 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+ 
     <RecoilRoot>
       <BrowserRouter>
         <AppWrapper />
       </BrowserRouter>
     </RecoilRoot>
-  </React.StrictMode>
 );
