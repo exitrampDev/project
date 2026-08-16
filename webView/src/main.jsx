45d277cb19f65726d15e5f50fc9207f34577e658
Muhammad Taha <mtaha.dev@gmail.com>
2026-08-16 17:07:01 +0500

    work for pwa

--- webView/src/main.jsx
+++ webView/src/main.jsx
@@ -3,7 +3,19 @@ import ReactDOM from "react-dom/client";
 import AppWrapper from "./AppWrapper";
 import { RecoilRoot } from "recoil";
 import { BrowserRouter } from "react-router-dom";
+import { registerSW } from "virtual:pwa-register";
 import "./index.css";
+
+
+registerSW({
+  onNeedRefresh() {
+    console.log("New version available");
+  },
+  onOfflineReady() {
+    console.log("App is ready to work offline");
+  },
+});
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
--- webView/vite.config.js
+++ webView/vite.config.js
@@ -1,10 +1,51 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from "vite-plugin-pwa";
+
 
 // https://vite.dev/config/
 export default defineConfig({
   //  base: '/exitramp/',
-   plugins: [react()],
+   plugins: [
+    react(),
+
+    VitePWA({
+        registerType: "autoUpdate",
+
+        manifest: {
+          name: "Exit Ramp",
+          short_name: "Exit Ramp",
+          description: "Exit Ramp Application",
+          theme_color: "#ffffff",
+          background_color: "#ffffff",
+          display: "standalone",
+          start_url: "/",
+          scope: "/",
+
+          icons: [
+            {
+              src: "/android-chrome-192x192.png",
+              sizes: "192x192",
+              type: "image/png",
+            },
+            {
+              src: "/android-chrome-512x512.png",
+              sizes: "512x512",
+              type: "image/png",
+            },
+            {
+              src: "/android-chrome-512x512.png",
+              sizes: "512x512",
+              type: "image/png",
+              purpose: "any maskable",
+            },
+          ],
+        },
+        workbox: {
+        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
+      },
+      }),
+    ],
    server: {
     host: '0.0.0.0',
     allowedHosts: ['exitramp.co', 'test.exitramp.co'],
